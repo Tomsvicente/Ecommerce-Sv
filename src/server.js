a85770dbf406353df18dedbe3c2948a019c0f24a
Tomas <tomsebvicente@gmail.com>
2022-06-23 21:11:10 -0300

    PUG&EJS

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+const main = async ()=>{
+    await app.listen(app.get('port'))
+    console.log(
+        `Servidor HTTP conectado, escuchando en el puerto ${app.get('port')}`
+    );
+}
+
+main()
